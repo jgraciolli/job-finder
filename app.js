97d6ba25658c787215c1218aae926c113fc4a862
jgraciolli <148507173+jgraciolli@users.noreply.github.com>
2025-05-29 11:31:16 -0300

    feat: first GET and POST route for jobs

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const db = require('./db/connection')
+const bodyParser = require('body-parser')
 
-//start server
+// start server
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
+    console.log(`Server listening port ${PORT}`)
 })
 
-//connect database
+// add body-parser tool
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// connect database
 db.authenticate()
-    .then(console.log('Conectou com sucesso!'))
+    .then(() => {
+        console.log('Connected to the database')
+    })
     .catch(err => {
-        console.log('Deu errinhonn')
+        console.log(`Error when connecting to the database: ${err.message}`)
     })
 
+// ---ROUTES SECTION---
+
+// main route
 app.get('/', (req, res) => {
-    res.send('Pagina testezaothefatinhon')
+    res.send('Main page')
 })
 
+// jobs routes
+app.use('/jobs', require('./routes/jobs'))
---
+++ routes/jobs.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const Job = require('../models/Job')
+
+router.get('/getAll', (req, res) => {
+    console.log('Found all jobs')
+})
+
+router.post('/add', (req, res) => {
+    console.log(req.body)
+    let {title, description, salary, company, email, new_job} = req.body
+    console.log({title, description, salary, company, email, new_job})
+
+    Job.create({
+        title, 
+        description, 
+        salary, 
+        company, 
+        email, 
+        new_job
+    })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+})
+
+module.exports = router
