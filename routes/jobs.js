55daa55499fb7d66fbd3c58412231ae07f8a3c57
jgraciolli <148507173+jgraciolli@users.noreply.github.com>
2025-06-01 12:41:43 -0300

    feat: final project adjustments
    also populated database fields with gpt generated job positions

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,14 +2,22 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
 
+router.get('/view/:id', (req, res) => { 
+    const id = req.params.id
+
+    Job.findByPk(id)
+    .then((job) => {    
+        res.render('view', {job})
+    })
+    .catch(err => console.log(err))
+})
+
 router.get('/add', (req, res) => {
     res.render('add')
 })
 
-router.post('/add', (req, res) => {
-    console.log(req.body)
+router.post('/add', (req, res) => {  
     let {title, description, salary, company, email, new_job} = req.body
-    console.log({title, description, salary, company, email, new_job})
 
     Job.create({
         title, 
@@ -21,10 +29,6 @@ router.post('/add', (req, res) => {
     })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
-})
-
-router.get('/view/:id', (req, res) => {
-    res.render('view')
 })
 
 module.exports = router
